34f475a873b947fe42e7de7c193ccf06735626a7
nicojrz <juarez.barrios.axel.nicolas@gmail.com>
2025-04-07 14:51:41 -0600

    squeue

--- squeue/squeue.c
+++ squeue/squeue.c
@@ -1,1 +1,103 @@
-isa
+#include "squeue.h"
+
+int next(int n)
+{
+	return (n+1)%SIZE;
+}
+
+void create_queue(Queue* q)
+{
+	q->head = -1;
+	q->tail = -1;
+}
+
+int isFull_queue(Queue q)
+{
+	if(next(q.tail) == q.head)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+int isEmpty_queue(Queue q)
+{
+	if(q.head == -1)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+void enqueue_queue(Queue* q, element e)
+{
+	if(isFull_queue(*q))
+	{
+		printf("Error de overflow\n");
+	}
+	else
+	{
+		if(isEmpty_queue(*q))
+		{
+			q->head = next(q->head);
+		}
+		q->tail = next(q->tail);
+		q->Q[q->tail] = e;
+	}
+}
+
+element dequeue_queue(Queue* q)
+{
+	element e
+	if(isEmpty_queue(*q))
+	{
+		e = -1;
+		printf("Error de underflow\n");
+	}
+	else
+	{
+		e = q->Q[q->head];
+		if(q->tail == q->head)
+		{
+			q->tail = -1;
+			q->head = -1;
+		}
+		else
+		{
+			q->head = next(q->head);
+		}
+	}
+	return e;
+}
+
+void delete_queue(Queue* q)
+{
+	while(!isEmpty_queue(*q))
+	{
+		element e = dequeue_queue(q);
+	}
+}
+
+void copy_queue(Queue* q, Queue* c)
+{
+	element e;
+	Queue aux;
+	create_queue(&aux);
+
+	while(!isEmpty_queue(*q))
+	{
+		enqueue_queue(&aux, dequeue_queue(q));
+	}
+	while(!isEmpty_queue(aux))
+	{
+		e = dequeue_queue(&aux);
+		enqueue_queue(q, e);
+		enqueue_queue(c, e);
+	}
+}
--- squeue/squeue.h
+++ squeue/squeue.h
@@ -1,1 +1,28 @@
-a
+#ifndef SQUEUE
+#define SQUEUE
+
+#define SIZE 10
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int element;
+typedef struct Queue
+{
+	element Q[SIZE];
+	int head;
+	int tail;
+}
+Queue;
+
+int next(int n);
+
+void create_queue(Queue* q);
+int isFull_queue(Queue q);
+int isEmpty_queue(Queue q);
+void enqueue_queue(Queue* q, element e);
+element dequeue_queue(Queue* q);
+void delete_queue(Queue* q);
+void copy_queue(Queue* q, Queue* c);
+
+#include "squeue.c"
+#endif
