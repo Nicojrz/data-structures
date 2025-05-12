1cd70348b79cf0876223776672ccd2e276a4aab1
nicojrz <juarez.barrios.axel.nicolas@gmail.com>
2025-05-12 13:55:41 -0600

    dlist header

--- dlist/dlist.h
+++ dlist/dlist.h
@@ -1,1 +1,37 @@
-hola
+#ifndef DLIST
+#define DLIST
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int element;
+
+typedef struct Node
+{
+	element e;
+	struct Node *prev;
+	struct Node *next;
+} Node;
+
+typedef struct DList
+{
+	Node *head;
+	Node *tail;
+} DList;
+
+void create_dlist(DList *l);
+int isEmpty_dlist(Dlist l);
+void insertFirst_dlist(Dlist *l, element e);
+void insertEnd_dlist(Dlist *l, element e);
+void insertAfter_dlist(Dlist *l, element e, int i);
+element deleteFirst_dlist(DList *l);
+element deleteEnd_dlist(DList *l);
+element deleteNode_dlist(DList *l, int i);
+void delete_dlist(DList *l);
+element searchNode_dlist(DList *l, int i);
+int searchElement_dlist(DList *l, element e);
+void print__dlist(DList *l);
+
+#include "dlist.c"
+
+#endif
